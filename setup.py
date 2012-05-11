9f0f2c72213e4c93d40b7978d44b67e7b65cdcec
Tim Henderson <tim.tadh@gmail.com>
2012-05-11 15:07:51 -0400

    roughed in structure
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='optutils',
+      version='git master',
+      description=(
+        'Small, composable utilities for writing great command line tools.'
+      ),
+      author='Tim Henderson',
+      author_email='tim.tadh@gmail.com',
+      url='https://www.github.com/timtadh/optutils',
+      license='GPL',
+      packages=['optutils'],
+)
