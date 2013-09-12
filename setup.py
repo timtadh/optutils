123a37544d71ec9de734e1351a04b2b67743fe34
Tim Henderson <tim.tadh@gmail.com>
2013-09-12 16:14:39 -0400

    .01 release
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='optutils',
-      version='git master',
+      version='.01',
       description=(
         'Small, composable utilities for writing great command line tools.'
       ),
